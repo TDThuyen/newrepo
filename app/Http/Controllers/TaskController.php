cbe62fed50e017f9cdc7e001954625ff32ee4ef0
TDThuyen <22026505@vnu.edu.vn>
2025-08-04 17:54:18 +0700

    chapter 12

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
@@ -12,7 +15,12 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return Task::all();
+        $tasks = DB::table('tasks')
+            ->join('users', 'tasks.user_id', '=', 'users.id')
+            ->select('tasks.*', 'users.username as user_username')
+            ->paginate(10);
+
+        return view('tasks.index', compact('tasks'));
     }
 
     /**
@@ -26,9 +34,17 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        DB::table('tasks')->insert([
+            'name' => $request->name,
+            'user_id' => $request->user_id,
+            'completed' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return redirect()->route('tasks.index')->with('success', 'Task created with Query Builder.');
     }
 
     /**
@@ -50,16 +66,25 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Task $task)
+    public function update(UpdateTaskRequest $request, $id)
     {
-        //
+        DB::table('tasks')
+            ->where('id', $id)
+            ->update([
+                'name' => $request->name,
+                'completed' => $request->completed,
+                'updated_at' => now(),
+            ]);
+
+        return redirect()->route('tasks.index')->with('success', 'Task updated with Query Builder.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy($id)
     {
-        //
+        DB::table('tasks')->where('id', $id)->delete();
+        return redirect()->route('tasks.index')->with('success', 'Task deleted with Query Builder.');
     }
 }
